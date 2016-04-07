6abe6efcc09d5b005f073f28e176661bf8e7ada1
Vincent Khougaz <vincent@khougaz.com>
2016-04-07 15:35:14 -0700

    Updated portfolio html, vendored CDN assets, updated theme

--- content/portfolio/assets/js/main.js
+++ content/portfolio/assets/js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "http://feeds.feedburner.com/TechCrunch/startups",
+        "http://avalarky.report/feeds/vincent-khougaz.rss.xml",
         
         {
         // how many entries do you want?
@@ -55,11 +55,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
+    GitHubCalendar("#github-graph", "sdobz");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "sdobz", selector: "#ghfeed" });
 
 
 });
